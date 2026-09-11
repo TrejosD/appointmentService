028e5466189cfcaa96556f1e3d1264ff96b3601c
Diego Trejos <d.trejosh@gmail.com>
2026-09-11 09:18:12 -0600

    actualizado createAppointmentDto

--- src/appointment/dto/create-appointment.dto.ts
+++ src/appointment/dto/create-appointment.dto.ts
@@ -1,7 +1,14 @@
+import { IsString } from 'class-validator';
+
 export class CreateAppointmentDto {
+  @IsString()
   bussinessID: string;
+  @IsString()
   customerID: string;
+  @IsString()
   productID: string;
+  @IsString()
   dayDate: Date;
+  @IsString()
   startTime: Date;
 }
