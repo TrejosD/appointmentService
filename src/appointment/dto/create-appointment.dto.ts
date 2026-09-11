0402043c54be89de59f1eac28067b871e6706e61
Diego Trejos <d.trejosh@gmail.com>
2026-09-10 20:48:13 -0600

    se remueve el string validator createAppDto

--- src/appointment/dto/create-appointment.dto.ts
+++ src/appointment/dto/create-appointment.dto.ts
@@ -1,14 +1,7 @@
-import { IsString } from 'class-validator';
-
 export class CreateAppointmentDto {
-  @IsString()
   bussinessID: string;
-  @IsString()
   customerID: string;
-  @IsString()
   productID: string;
-  @IsString()
   dayDate: Date;
-  @IsString()
   startTime: Date;
 }
