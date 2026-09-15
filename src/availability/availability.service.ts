ba0b53b893f32938e846ac9bb22967f541bbabe7
Diego Trejos <d.trejosh@gmail.com>
2026-09-14 18:11:45 -0600

    string type 2.0

--- src/availability/availability.service.ts
+++ src/availability/availability.service.ts
@@ -179,11 +179,11 @@ export class AvailabilityService {
       return spaceTime;
     } catch (error) {
       if (error instanceof Error) {
-        if (error.name.toString() === 'BadRequestException') {
+        if (error.name == 'BadRequestException') {
           console.log('Error fue badRequest');
           throw new BadRequestException(error.message);
         }
-        if (error.name.toString() === 'NotFoundException') {
+        if (error.name == 'NotFoundException') {
           console.log('Error fue notFound');
           throw new NotFoundException(error.message);
         }
