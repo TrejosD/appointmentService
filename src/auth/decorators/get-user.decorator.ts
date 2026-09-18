aa79bfe1f0588c7a7990973a75a1901f8a96a3f4
Diego Trejos <d.trejosh@gmail.com>
2026-09-18 12:13:27 -0600

    set data == email to return

--- src/auth/decorators/get-user.decorator.ts
+++ src/auth/decorators/get-user.decorator.ts
@@ -14,9 +14,10 @@ export const GetUser = createParamDecorator((data, ctx: ExecutionContext) => {
   const user: User = req.user;
   // si no encontramos el user, mostramos el error, si esta lo retornamos
   if (!user) throw new InternalServerErrorException('User not found (request)');
-  console.log('Impresion del data');
-  console.log(data);
   if (data == null) {
     return user;
+  }
+  if (data == 'email') {
+    return user.email;
   }
 });
