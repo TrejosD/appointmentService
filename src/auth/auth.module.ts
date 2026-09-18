39970237012837f1e12ee8c95f6f869a45ae7260
Diego Trejos <d.trejosh@gmail.com>
2026-09-18 11:39:12 -0600

    se inclute JWTStrategy dentro de los imports

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,20 +6,19 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './entities/user.entity';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
   imports: [
     ConfigModule,
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
-    PassportModule,
+    PassportModule.register({ defaultStrategy: 'jwt' }),
     JwtModule.registerAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: () => {
-        console.log('ENV secret');
-        console.log(process.env.JWT_SECRET);
         return {
           secret: process.env.JWT_SECRET,
           signOptions: {
@@ -29,6 +28,6 @@ import { JwtModule } from '@nestjs/jwt';
       },
     }),
   ],
-  exports: [AuthService, PassportModule, JwtModule],
+  exports: [AuthService, PassportModule, JwtModule, JwtStrategy],
 })
 export class AuthModule {}
