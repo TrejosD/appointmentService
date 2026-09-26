6b9b06a8bbdf0a016668e9b6b3d9e60d3b7d3117
Diego Trejos <d.trejosh@gmail.com>
2026-09-26 07:12:22 -0600

    listas imports N moduls, metodo liberar espacio cita

--- src/notifications/notifications.module.ts
+++ src/notifications/notifications.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { NotificationsService } from './notifications.service';
 import { NotificationsController } from './notifications.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserDevice, UserDeviceSchema } from './entities/user-device.entity';
 
 @Module({
   controllers: [NotificationsController],
   providers: [NotificationsService],
+  imports: [
+    MongooseModule.forFeature([
+      { name: UserDevice.name, schema: UserDeviceSchema },
+    ]),
+  ],
+  exports: [NotificationsService, MongooseModule],
 })
 export class NotificationsModule {}
