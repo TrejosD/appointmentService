ddf5a3136781e44b4c8e7498d8cfd61df161c774
Diego Trejos <d.trejosh@gmail.com>
2026-09-26 09:03:14 -0600

    removed firebase module

--- src/firebase/firebase.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module, Global } from '@nestjs/common';
-import { FirebaseService } from './firebase.service';
-
-@Global()
-@Module({
-  providers: [FirebaseService],
-  exports: [FirebaseModule],
-})
-export class FirebaseModule {}
