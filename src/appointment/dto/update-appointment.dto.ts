41faf7a00cdbd7026f113efc5185b407aba67e59
Diego Trejos <d.trejosh@gmail.com>
2026-09-26 06:58:49 -0600

    metodo liberar espacio de cita

--- src/appointment/dto/update-appointment.dto.ts
+++ src/appointment/dto/update-appointment.dto.ts
@@ -1,7 +1,4 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateAppointmentDto } from './create-appointment.dto';
-import { AppointmentStatus } from '../entities/appointment.entity';
 
-export class UpdateAppointmentDto extends PartialType(CreateAppointmentDto) {
-  status: AppointmentStatus;
-}
+export class UpdateAppointmentDto extends PartialType(CreateAppointmentDto) {}
